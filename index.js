87c8e5281bba9607629280a752dd10f1b4d839ad
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-13 09:45:08 +0300

    second try

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
 console.log('about to fetch a character');
-fetch('finn.jpg').then(response=>{
+fetch('finn.jpg')
+.then(response=>{
     console.log(response);
+    // convert response into img blob
+    return response.blob();
+})
+.then(blob=>{
+    console.log(blob);
+    document.getElementById('finn').src = blob;
 });
